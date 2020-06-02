a93c37c51710d2c7b4d2a39dbcec31a8a8c2e1a3
Jassu Sharma <jassusharma660@gmail.com>
2020-06-02 23:29:19 +0530

    Added fixed error banner.

--- core/config.php
+++ core/config.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(0);
+//error_reporting(0);
 define('DB_SERVER','localhost');
 define('DB_USERNAME','seeyou_dba');//seeyou_dba
 define('DB_PASSWORD','You_Password_123');//You_Password_123
@@ -8,4 +8,4 @@ define('DB_NAME','seeyouuserdb');
 $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 if($con === false)
-  echo ("<div style='color:#fff;background-color:#B71C1C;padding: 0.5%;'>Error: DbCnnctErr, Contact Administrator!</div>");
+  echo ("<div style='color:#fff;background-color:#B71C1C;padding: 0.5%;position:fixed;width:100%;'>Error: DbCnnctErr, Contact Administrator!</div>");
