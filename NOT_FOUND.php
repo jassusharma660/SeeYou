8bcadfaf0616666be4dfd2a31e2572307049db91
Jassu Sharma <jassusharma660@gmail.com>
2020-06-05 21:40:26 +0530

    Create NOT_FOUND.php

---
+++ NOT_FOUND.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+  <title>Oh No!</title>
+  <style>
+    body {
+      text-align:center;
+      background:#eee;
+      font-family:Arial;
+    }
+    main {
+      position:relative;
+      top:50%;
+      transform:translateY(-50%);
+    }
+    .bigger {
+      font-size: 5em;
+      font-weight: bold;
+    }
+    .big {
+      font-weight: bold;
+      color:#777;
+      font-size:4em;
+    }
+    a {
+      color: #000;
+    }
+  </style>
+</head>
+<body>
+  <main>
+    <div class="bigger">
+      &#9869; <br/>
+      404
+    </div>
+    <div class="big">ERROR</div>
+    <a href="./">Take me back to HOME</a>
+  </main>
+</body>
+</html>
