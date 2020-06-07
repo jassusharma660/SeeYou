86ebc7079c1f23450e6a2625ace4ba1c3da63d10
Jassu Sharma <jassusharma660@gmail.com>
2020-06-07 21:45:03 +0530

    Update header.php

--- views/header.php
+++ views/header.php
@@ -7,55 +7,24 @@
     </section>
   </a>
   <section class="options container">
-    <img src="../assets/images/icons/notification-50.png" style="height:116%;" alt="notifications" class="notification" onclick="window.location.href='./notification.php';">
-    <span class="alert"></span>
-    <img src="../assets/images/icons/speech-bubble-50.png" alt="messages" class="message" onclick="window.location.href='./message.php';">
-    <span class="alert"></span>
+    <!--img src="../assets/images/icons/notification-50.png" style="height:116%;" alt="notifications" class="notification" onclick="window.location.href='./notification.php';">
+    <span class="alert"></span-->
+    <!--img src="../assets/images/icons/speech-bubble-50.png" alt="messages" class="message" onclick="window.location.href='./message.php';">
+    <span class="alert"></span-->
   </section>
-  <section class="search container">
+  <section class="search container" id="search_container">
     <form action="" method="post" onsubmit="return false">
       <button type="submit"></button>
-      <input type="text" name="query" placeholder="Search" autocomplete="off" onfocus="liveSearch()" onblur="liveSearch(true)">
+      <input type="text" name="query" id="search_query" placeholder="Search" autocomplete="off" onkeyup="liveSearch(false)">
     </form>
     <span class="header_divider"></span>
     <img src="../assets/images/icons/settings-50.png" alt="setting" class="setting" onclick="window.location.href='./setting.php';">
+    <img src="../assets/images/icons/logout-rounded-up-50.png" alt="logout" class="logout" onclick="window.location.href='./logout.php';">
+
   </section>
 
   <div id="search_result" class="scrollbar">
-    <a href="./view_profile.php">
-      <div class="friend">
-        <img src="../usr_data/profile_pic/100x100/google.jpg" alt="user_dp" class="user_dp">
-        google
-      </div>
-    </a>
 
-    <a href="./view_profile.php">
-      <div class="friend">
-        <img src="../usr_data/profile_pic/100x100/deepaksharma550.jpg" alt="user_dp" class="user_dp">
-        deepaksharma550
-      </div>
-    </a>
-
-    <a href="./view_profile.php">
-      <div class="friend">
-        <img src="../usr_data/profile_pic/100x100/yogita_sharma28.jpg" alt="user_dp" class="user_dp">
-        yogita_sharma28
-      </div>
-    </a>
-
-    <a href="./view_profile.php">
-      <div class="friend">
-        <img src="../usr_data/profile_pic/100x100/aman_rai___.jpg" alt="user_dp" class="user_dp">
-        aman_rai___
-      </div>
-    </a>
-
-    <a href="./view_profile.php">
-      <div class="friend">
-        <img src="../usr_data/profile_pic/100x100/j_for_jassu.jpg" alt="user_dp" class="user_dp">
-        j_for_jassu
-      </div>
-    </a>
   </div>
 
 </header>
